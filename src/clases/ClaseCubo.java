13204b808a02debe73837a6711c9522d1050941c
wbmcb <wbmcb@DESKTOP-DNRTVEC>
2021-06-08 00:08:15 -0500

    se cambió el mensaje en la interfaz del usuario 1

--- src/clases/ClaseCubo.java
+++ src/clases/ClaseCubo.java
@@ -22,7 +22,7 @@ int a, h, p;
         
         ClaseCubo ob1 = new ClaseCubo();
         resultado = ob1.calculavolumen(3, 2, 6);
-        System.out.println("El volumen es ="+resultado);
+        System.out.println("El volumen es = "+resultado);
         
    
         
